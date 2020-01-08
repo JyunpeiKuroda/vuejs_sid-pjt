ce8be547ef49457bd2ecc09d0e34106119e93019
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-01-08 17:48:22 +0900

    #1sample-axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    dataSet: {}
   },
   mutations: {
+    mutateDataSet(state, payload) {
+      state.dataSet = payload;
+    }
   },
   actions: {
+    commitDataSet(store) {
+      return axios.get('./data/sample.json')
+      .then(response => {
+        store.commit('mutateDataSet', response.data)
+      })
+      .catch((reason) => {
+        console.log(reason.message)
+      })
+    }
+  },
+  getters: {
+    getStateDataSet: (state) => state.dataSet
   },
   modules: {
   }
