d71b836c46bd51ca288a487704f68a392944e504
JyunpeiKuroda <totoro.black6996@gmail.com>
2020-02-19 12:57:56 +0900

    #1add_to_Vuetify_Done
    Vuetifyの導入完了

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
 
 
 Vue.config.productionTip = false
@@ -10,6 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  Vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
